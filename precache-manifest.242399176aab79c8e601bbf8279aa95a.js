e9309e59242fadfaaec07b016462aa4c09e142d8
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-07-17 16:45:33 +0300

    Updates

---
+++ precache-manifest.242399176aab79c8e601bbf8279aa95a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c2941da22d5d863413db34043f3ab2",
+    "url": "/react-portal/index.html"
+  },
+  {
+    "revision": "d7220684097e2e68eef2",
+    "url": "/react-portal/static/css/main.1806c359.chunk.css"
+  },
+  {
+    "revision": "4a2880f2efa9989cdacf",
+    "url": "/react-portal/static/js/2.8d5374f0.chunk.js"
+  },
+  {
+    "revision": "d7220684097e2e68eef2",
+    "url": "/react-portal/static/js/main.fa0e56de.chunk.js"
+  },
+  {
+    "revision": "dfd4b6d058de9505cdad",
+    "url": "/react-portal/static/js/runtime~main.afff1fac.js"
+  }
+]);
